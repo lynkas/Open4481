ec95e47c3b5102a3fe70b6f56ee0fe1447511df7
lynkas <cat@catting.net>
2023-02-27 02:35:02 -0500

    fix de2

--- de_binary_arithmetic.py
+++ de_binary_arithmetic.py
@@ -4,7 +4,7 @@ getcontext().prec = 10
 d = Decimal
 
 
-def E1(crange,binary):
+def E1(crange, binary):
     changed = False
     new_crange = crange
     if crange[0] < 0.5 and crange[1] < 0.5:
@@ -14,10 +14,10 @@ def E1(crange,binary):
         if len(binary) == 0:
             binary = "0"
         binary = binary[1:]
-    return changed, new_crange,binary
+    return changed, new_crange, binary
 
 
-def E2(crange,binary):
+def E2(crange, binary):
     changed = False
     new_crange = crange
     if crange[0] >= 0.5 and crange[1] >= 0.5:
@@ -27,45 +27,43 @@ def E2(crange,binary):
         if len(binary) == 0:
             binary = "0"
         binary = binary[1:]
-    return changed, new_crange,binary
+    return changed, new_crange, binary
 
 
-def E3(crange,binary):
+def E3(crange, binary):
     changed = False
     new_crange = crange
     if 0.25 <= crange[0] < 0.5 <= crange[1] < 0.75:
         changed = True
-        new_crange = (d(0.5) - (d(0.5)-crange[0]) * 2, d(0.5) + (crange[1] - d(0.5)) * 2)
+        new_crange = (d(0.5) - (d(0.5) - crange[0]) * 2, d(0.5) + (crange[1] - d(0.5)) * 2)
         print(f"E3 -> [{new_crange[0]},{new_crange[1]})", end=" ")
-        binary = "1"+binary[2:]
-    return changed, new_crange,binary
+        binary = "1" + binary[2:]
+    return changed, new_crange, binary
 
 
 def decode(binary, tags, level):
     crange = (d(0), d(1))
-    value = b2f(binary)
 
     while level > 0:
         while True:
             for f in [E1, E2]:
-                changed, new_crange,binary = f(crange,binary)
+                changed, new_crange, new_binary = f(crange, binary)
                 if changed:
                     crange = new_crange
-                    value = b2f(binary)
-                    print(f"0.{binary}, i.e., {value}")
-                    value = (value - crange[0]) / (crange[1] - crange[0])
+                    binary = new_binary
+                    print(f"0.{binary}, i.e., {b2f(binary)}")
                     break
             else:
                 break
 
         for i, tag in enumerate(tags):
-            if tag > value:
+            if tag > (b2f(binary) - crange[0]) / (crange[1] - crange[0]):
                 level -= 1
                 start = tags[i - 1]
                 end = tags[i]
+                print(f"{(b2f(binary) - crange[0]) / (crange[1] - crange[0])} -> {i}", end="")
                 crange = (crange[0] + (crange[1] - crange[0]) * start, crange[0] + (crange[1] - crange[0]) * end)
-                print(f"{value} -> {i} -> [{crange[0]},{crange[1]})")
-                value = (value - crange[0]) / (crange[1] - crange[0])
+                print(f" -> [{crange[0]},{crange[1]})")
                 break
 
     print("=========")
@@ -82,14 +80,14 @@ def b2f(binary):
 
 
 if __name__ == '__main__':
-    # content = input("p ")
-    content = "0.8 0.02 0.18"
+    content = input("p ")
+    # content = "0.8 0.02 0.18"
     p = [d(x) for x in content.split()]
     tags = [d(0)]
     for i in p:
         tags.append(tags[-1] + i)
-    number = "1100011000001"
-    # number = input("number ")
-    level = 4
-    # level = int(input("level "))
+    # number = "1100011000001"
+    number = input("number ")
+    # level = 4
+    level = int(input("level "))
     decode(number, tags, level)
